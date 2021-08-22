c51a8816443c51d6d195ac9dbb34e67c7e0970a8
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-08-22 23:43:45 +0900

    [init] init project
    프로젝트 초기 설정

---
+++ src/main/java/com/map/begonevegan/BegoneveganApplication.java
@@ -0,0 +1,13 @@
+package com.map.begonevegan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BegoneveganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BegoneveganApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/map/begonevegan/BegoneveganApplicationTests.java
@@ -0,0 +1,13 @@
+package com.map.begonevegan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BegoneveganApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
